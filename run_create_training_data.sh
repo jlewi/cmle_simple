9bba9dd99b291794937e0ee019abd880bf7cd459
Jeremy Lewi <jlewi@google.com>
2017-08-26 17:56:26 -0700

    * Delete bash scripts. The datalab notebook is the preferred way to do things.

--- run_create_training_data.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python -m running_average.create_training_data \
-    --output_path=gs://dataflow-jlewi_cloud_ml/running_average/input/inputs.tfrecord
--- run_online_predict.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ENDPOINT=https://cloudml-jlewi-test.sandbox.googleapis.com/v1alpha3
-#ENDPOINT=""https://cloudml-jlewi-test.sandbox.googleapis.com/v1alpha3""
-python -m running_average.online_predict \
-  --project_id=cloud-ml-dev \
-  --model=running_average \
-  --version=v0627_040112
